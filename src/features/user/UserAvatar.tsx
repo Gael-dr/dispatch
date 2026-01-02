510857244a142d597b97376e8964123dc670e59c
Gael-dr <gaelduclocher.rougelot@quantiq.io>
2026-01-02 10:30:06 +0100

    GA: feat: add avatar pic

---
+++ src/features/user/UserAvatar.tsx
@@ -0,0 +1,12 @@
+import { Avatar, AvatarFallback, AvatarImage } from '@/shared/ui/avatar'
+
+export function UserAvatar() {
+  return (
+    <div className="flex flex-row flex-wrap items-center gap-12">
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </div>
+  )
+}
