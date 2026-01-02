a80b29fd82988841fbb54af59d6c520717a09192
Gael-dr <gaelduclocher.rougelot@quantiq.io>
2026-01-02 19:54:47 +0100

    GA: feat: add navigation menu & settings page

--- src/app/router.tsx
+++ src/app/router.tsx
@@ -1,6 +1,7 @@
 import Dashboard from '@/pages/Dashboard'
 import Decisions from '@/pages/Decisions'
 import Home from '@/pages/Home'
+import Settings from '@/pages/Settings'
 import { createBrowserRouter } from 'react-router-dom'
 
 export const router = createBrowserRouter([
@@ -15,5 +16,9 @@ export const router = createBrowserRouter([
   {
     path: '/dashboard',
     element: <Dashboard />,
+  },
+  {
+    path: '/settings',
+    element: <Settings />,
   },
 ])
